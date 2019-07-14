6d29b2f64a699c5d7b702aeb3f1996e7e23388da
Amar kumar <32639666+amark720@users.noreply.github.com>
2019-07-14 10:40:05 +0530

    Add files via upload
    used Php, MySql, Javascript, Bootstrap

---
+++ Secret Diary (php+Mysql+JS)/connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    $link = mysqli_connect("localhost","id6596089_wp_663b30ab708a6382b4e5946c4c3de9aa","amark720","id6596089_wp_663b30ab708a6382b4e5946c4c3de9aa");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+
+?>
